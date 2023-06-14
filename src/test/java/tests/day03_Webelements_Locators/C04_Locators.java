b3541f6ba6a75d76a66de9003e28cbc58b06de11
firdevs19 <fiadhakeaf@gmail.com>
2023-06-14 23:33:50 +0200

    commit-2

--- src/test/java/tests/day03_Webelements_Locators/C04_Locators.java
+++ src/test/java/tests/day03_Webelements_Locators/C04_Locators.java
@@ -17,27 +17,46 @@ public class C04_Locators {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-
         // amazon anasayfaya gidin
-
         driver.get("https://www.amazon.com");
 
         // arama kutusunun yanindaki today's deals linkini tiklayin
 
-        WebElement todaysDealsElementi = driver.findElement(By.linkText("Today's Deals"));
-        todaysDealsElementi.click();
+        // WebElement todaysDealsElementi = driver.findElement(By.linkText("Today's Deals"));
+        // WebElement todaysDealsElementi = driver.findElement(By.partialLinkText("Deals"));
+        /* WebElement todaysDealsElementi = driver.findElement(By.partialLinkText("Today"));
+
+            todaysDealsElementi.click();
+
+            Bir web element sadece click yapmak icin kullanilacaksa
+            locate edip, bir webelemente kaydedip, sonra click yapmak yerine
+
+            atama yapmadan, locate ve click islemleri birlikte yapilabilir
+
+            ANCAK bu durumda kodun ne yaptigi sonradan bakanlar icin anlasilir olmayabilir
+            metin olarak aciklama olan durumlarda bu sekilde kullanim daha guzel olur
+         */
 
         Thread.sleep(2000);
+        driver.  findElement(By.partialLinkText("Tages")).click();  // Almancadan dolayi: "Angebot des Tages" - EN:Today's Deal.
+
 
         // acilan sayfada ilk urunu tiklayin
-
+        Thread.sleep(3000);
         List<WebElement> urunElementleriListesi = driver.findElements(By.className("DealGridItem-module__dealItemContent_1vFddcq1F8pUxM8dd9FW32"));
 
+        /*
+            bir locator sonuc olarak birden fazla webelement donduruyorsa
+            findElements() kullanirsak locator'a uygun olan tum WebElementleri getirirken,
+            findElement() kullanirsak locator'a uygun olan WebElement'lerin ILKINI bize getirir
+
+            driver.findElement(By.className(locateBilgisi)).click()
+         */
         urunElementleriListesi.get(0).click();
 
-
+        Thread.sleep(3000);
         driver.close();
-
-
     }
 }
+
+
---
+++ src/test/java/tests/day03_Webelements_Locators/Homework4.java
@@ -0,0 +1,62 @@
+package tests.day03_Webelements_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Homework4 {
+    public static void main(String[] args) throws InterruptedException {
+
+        /*
+            1.Bir class oluşturun : AmazonSearchTest
+2. Main method oluşturun ve aşağıdaki görevi tamamlayın.
+a.web sayfasına gidin. https://www.amazon.com/
+b.Search(ara) “city bike”
+c. Amazon'da görüntülenen ilgili sonuçların sayısını
+yazdırın
+d.Sonra karşınıza çıkan ilk sonucun resmine tıkla
+
+
+         */
+
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com/");
+
+        // b.Search(ara) “city bike”
+
+        WebElement aramaKutusuElementi = driver.findElement(By.id("twotabsearchtextbox"));
+
+        aramaKutusuElementi.sendKeys("city bike");
+        aramaKutusuElementi.submit();
+
+        // c. Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+
+        List<WebElement> urunSayisiList=driver.findElements(By.xpath("//div[@class='a-section aok-relative s-image-fixed-height']"));
+        // List<WebElement> urunSayisiList=driver.findElements(By.className("a-section aok-relative s-image-fixed-height"));
+         System.out.println("Goruntulenen Urun Sayisi : "+urunSayisiList.size());
+
+        // d.Sonra karşınıza çıkan ilk sonucun resmine tıkla
+
+        urunSayisiList.get(0).click();
+
+
+
+        Thread.sleep(1000);
+        driver.close();
+
+
+
+
+
+
+
+    }
+}
